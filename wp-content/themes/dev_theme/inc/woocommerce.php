a3fc24488b0c7c05e4e3a75721822dc8ad310513
longpv <long05062000@gmail.com>
2025-03-05 10:40:42 +0700

    trang checkout lựa chọn tỉnh thành việt nam

--- wp-content/themes/dev_theme/inc/woocommerce.php
+++ wp-content/themes/dev_theme/inc/woocommerce.php
@@ -310,7 +310,7 @@ function custom_related_products_section()
 				?>
 			</div>
 		</div>
-<?php
+	<?php
 	}
 	wp_reset_postdata();
 }
@@ -380,14 +380,16 @@ add_action('woocommerce_before_main_content', 'custom_breadcrumb_woo', 20);
 add_action('woocommerce_cart_calculate_fees', 'custom_shipping_fee_based_on_location');
 function custom_shipping_fee_based_on_location($cart_object)
 {
+	// tránh bị tấn công
 	if (is_admin() && !defined('DOING_AJAX')) {
 		return;
 	}
 
 	// Lấy thông tin địa chỉ khách hàng
-	$customer_country = WC()->customer->get_billing_country(); // Quốc gia
-	$customer_city    = WC()->customer->get_billing_city();    // Hà Nội, TP.HCM
-	$customer_state   = WC()->customer->get_billing_state();   // Quận/huyện
+	$customer_country   = WC()->customer->get_billing_country();  // Quốc gia
+	$customer_city      = WC()->customer->get_billing_city();     // Tỉnh/Thành phố
+	$customer_district  = WC()->customer->get_billing_address_1(); // Quận/Huyện
+	$customer_ward      = WC()->customer->get_billing_address_2(); // Phường/Xã (chưa dùng, nhưng có thể mở rộng)
 
 	// Nếu khách không ở Việt Nam => phí ship 200$
 	if ($customer_country != 'VN') {
@@ -395,26 +397,136 @@ function custom_shipping_fee_based_on_location($cart_object)
 		return; // Không kiểm tra tiếp các điều kiện khác
 	}
 
-	// Danh sách quận trung tâm của Hà Nội và TP.HCM
-	$hn_central_districts = ['Ba Đình', 'Hoàn Kiếm'];
-	$hcm_central_districts = ['Quận 1', 'Quận 2'];
+	// Danh sách quận trung tâm của Hà Nội
+	$hn_central_districts = ['Quận Ba Đình', 'Quận Hoàn Kiếm'];
 
 	// Mặc định phí ship
 	$shipping_fee = 10; // Các tỉnh thành khác: 10$
 
-	// Nếu khách ở Hà Nội hoặc TP.HCM
-	if ($customer_city == 'Hà Nội' || $customer_city == 'HCM') {
+
+	if ($customer_city == 'Thành phố Hà Nội') {
 		$shipping_fee = 5; // Mặc định phí ship nội thành
 
-		// Nếu khách ở quận trung tâm
-		if (
-			($customer_city == 'Hà Nội' && in_array($customer_state, $hn_central_districts)) ||
-			($customer_city == 'TP.HCM' && in_array($customer_state, $hcm_central_districts))
-		) {
-			$shipping_fee = 3; // Phí thấp hơn nếu ở quận trung tâm
+		// Phí thấp hơn nếu ở quận trung tâm
+		if (in_array($customer_district, $hn_central_districts)) {
+			$shipping_fee = 3;
+		}
+
+		// Nếu khách ở Trung tâm và "Phường Hàng Mã" => Free ship
+		if ($customer_district == "Quận Hoàn Kiếm" && $customer_ward == 'Phường Hàng Mã') {
+			$cart_object->add_fee(__('Miễn phí vận chuyển', 'woocommerce'), 0, false);
+			return;
 		}
 	}
 
 	// Áp dụng phí vào giỏ hàng
 	$cart_object->add_fee(__('Phí vận chuyển', 'woocommerce'), $shipping_fee, false);
 }
+// end
+
+// lựa chọn tỉnh thành/quận huyện ở Việt Nam ở trang checkout để tính ship
+function select_province_and_district_vietnam_checkout()
+{
+	if (is_checkout()) {
+	?>
+		<script>
+			jQuery(document).ready(function($) {
+				// Tìm các ô input mặc định của WooCommerce
+				var cityInput = $("#billing_city");
+				var districtInput = $("#billing_address_1");
+				var wardInput = $("#billing_address_2");
+
+				// Thay thế input bằng select
+				cityInput.replaceWith('<select name="billing_city" id="billing_city" class="select2"><option value="">Chọn tỉnh/thành</option></select>');
+				districtInput.replaceWith('<select name="billing_address_1" id="billing_address_1" class="select2"><option value="">Chọn quận/huyện</option></select>');
+				wardInput.replaceWith('<select name="billing_address_2" id="billing_address_2" class="select2"><option value="">Chọn phường/xã</option></select>');
+
+				// Gán lại biến sau khi thay thế
+				var citis = $("#billing_city");
+				var districts = $("#billing_address_1");
+				var wards = $("#billing_address_2");
+
+				// Áp dụng Select2 để giao diện đẹp hơn
+				citis.select2({
+					placeholder: "Chọn tỉnh/thành",
+					width: '100%'
+				});
+				districts.select2({
+					placeholder: "Chọn quận/huyện",
+					width: '100%'
+				});
+				wards.select2({
+					placeholder: "Chọn phường/xã",
+					width: '100%'
+				});
+
+				// Gọi API lấy dữ liệu tỉnh/thành
+				$.ajax({
+					url: "https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json",
+					method: "GET",
+					dataType: "json",
+					success: function(data) {
+						renderCity(data);
+					},
+					error: function() {
+						console.log("Không thể tải dữ liệu địa lý!");
+					}
+				});
+
+				function renderCity(data) {
+					citis.empty().append('<option value="">Chọn tỉnh/thành</option>');
+					districts.empty().append('<option value="">Chọn quận/huyện</option>');
+					wards.empty().append('<option value="">Chọn phường/xã</option>');
+
+					$.each(data, function(index, item) {
+						citis.append($('<option>', {
+							value: item.Name,
+							text: item.Name,
+							'data-id': item.Id
+						}));
+					});
+
+					citis.change(function() {
+						var cityID = $(this).find(':selected').data("id");
+						districts.empty().append('<option value="">Chọn quận/huyện</option>');
+						wards.empty().append('<option value="">Chọn phường/xã</option>');
+
+						if (cityID) {
+							var selectedCity = data.find(n => n.Id == cityID);
+							$.each(selectedCity.Districts, function(index, item) {
+								districts.append($('<option>', {
+									value: item.Name,
+									text: item.Name,
+									'data-id': item.Id
+								}));
+							});
+						}
+						districts.trigger('change'); // Cập nhật select2
+					});
+
+					districts.change(function() {
+						var districtID = $(this).find(':selected').data("id");
+						wards.empty().append('<option value="">Chọn phường/xã</option>');
+
+						if (districtID) {
+							var cityID = citis.find(':selected').data("id");
+							var selectedCity = data.find(n => n.Id == cityID);
+							var selectedDistrict = selectedCity.Districts.find(n => n.Id == districtID);
+
+							$.each(selectedDistrict.Wards, function(index, item) {
+								wards.append($('<option>', {
+									value: item.Name,
+									text: item.Name
+								}));
+							});
+						}
+						wards.trigger('change'); // Cập nhật select2
+					});
+				}
+			});
+		</script>
+<?php
+	}
+}
+add_action('wp_footer', 'select_province_and_district_vietnam_checkout');
+// end
